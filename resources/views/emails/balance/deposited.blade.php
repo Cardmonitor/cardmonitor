dcee7882749178c55d36012809c8e05659c1fe61
danielsundermeier <daniel@hof-sundermeier.de>
2023-09-24 05:25:42 +0200

    drop balance

--- resources/views/emails/balance/deposited.blade.php
+++
@@ -1,8 +0,0 @@
-@component('mail::message')
-Hallo {{ $balance->user->name }},
-
-Wir haben soeben das Guthaben für dein Cardmonitor Konto mit {{ $balance->amount_in_euro_formatted }} € aufgeladen.<br />
-Dein Guthaben beträgt damit nun {{ $balance->user->balance_in_euro_formatted }} €.
-
-{{ config('app.name') }}
-@endcomponent
