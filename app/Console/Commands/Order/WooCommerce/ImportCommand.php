2dcdf9aff9d6a7aa3e200a67fb34cefdb0c45cbb
danielsundermeier <daniel@hof-sundermeier.de>
2024-02-18 04:50:44 +0100

    #233 WooCommerce ORder Import refactor
    - handle faild responses
    - renaming
    - refactor to functions

--- app/Console/Commands/Order/WooCommerce/ImportCommand.php
+++ app/Console/Commands/Order/WooCommerce/ImportCommand.php
@@ -7,8 +7,10 @@ use App\Enums\Orders\Status;
 use App\Models\Orders\Order;
 use Illuminate\Console\Command;
 use App\Support\BackgroundTasks;
+use Illuminate\Support\Collection;
 use App\Notifications\FlashMessage;
 use Illuminate\Support\Facades\App;
+use PhpParser\ErrorHandler\Collecting;
 use App\Enums\ExternalIds\ExternalType;
 use App\Importers\Orders\WooCommerceOrderImporter;
 
@@ -26,57 +28,86 @@ class ImportCommand extends Command
     public function handle(BackgroundTasks $BackgroundTasks)
     {
         $this->user = User::findOrFail($this->argument('user'));
+        $backgroundtask_key = 'user.' . $this->user->id . '.order.sync';
 
         try {
-            if (!$this->option('order')) {
-                $backgroundtask_key = 'user.' . $this->user->id . '.order.sync';
-                $BackgroundTasks->put($backgroundtask_key, 1);
-            }
+            $this->putBackgroundTask($BackgroundTasks, $backgroundtask_key);
 
             $woocommerce_states = $this->option('states');
-            $order_source_ids = collect();
-            foreach ($woocommerce_states as $woocommerce_state) {
-                $state = Status::fromWooCommerceSlug($woocommerce_state)->value;
-                $orders = Order::where('user_id', $this->user->id)->where('source_slug', ExternalType::WOOCOMMERCE->value)->state($state)->get();
-                $order_source_ids->concat($orders->pluck('source_id'));
-            }
+            $order_source_ids = $this->getOrderSourceIdsForStates($woocommerce_states);
 
             $orders = $this->getWooCommerceOrders();
-            $synced_orders = [];
+            $imported_order_source_ids = [];
             foreach ($orders as $order) {
                 $order = WooCommerceOrderImporter::import($this->user->id, $order);
-                $synced_orders[] = $order->source_id;
+                $imported_order_source_ids[] = $order->source_id;
             }
 
-            $not_synced_orders = $order_source_ids->diff($synced_orders);
-            foreach ($not_synced_orders as $woocommerce_order_id) {
-                $order = WooCommerceOrderImporter::import($this->user->id, $this->getWooCommerceOrder($woocommerce_order_id));
-            }
-
-            if (!$this->option('order')) {
-                $BackgroundTasks->forget($backgroundtask_key);
-                $this->user->notify(FlashMessage::success('Die WooCommerce Bestellungen im Status <b>' . implode(', ', $woocommerce_states) . '</b> wurden synchronisiert.', [
-                    'background_tasks' => App::make(BackgroundTasks::class)->all(),
-                ]));
-            }
+            $this->importNotImportedOrders($order_source_ids->diff($imported_order_source_ids));
+            $this->notifyUserSuccess($BackgroundTasks, $backgroundtask_key, $woocommerce_states);
 
             return self::SUCCESS;
         }
         catch (\Exception $e) {
             $BackgroundTasks->forget($backgroundtask_key);
-            $this->user->notify(FlashMessage::danger('Die WooCommerce Bestellungen im Status <b>' . implode(', ', $woocommerce_states) . '</b> konnten nicht synchronisiert werden.', [
+            $this->user->notify(FlashMessage::danger('Die WooCommerce Bestellungen im Status <b>' . implode(', ', $woocommerce_states) . '</b> konnten nicht importiert werden.', [
                 'background_tasks' => App::make(BackgroundTasks::class)->all(),
             ]));
             return self::FAILURE;
         }
     }
 
-    private function getWooCommerceOrder(int $woocommerce_order_id): array
+    private function putBackgroundTask(BackgroundTasks $BackgroundTasks, string $backgroundtask_key): void
     {
-        $WooCommerce = new \App\APIs\WooCommerce\WooCommerceOrder();
-        $response = $WooCommerce->order($woocommerce_order_id);
+        if ($this->option('order')) {
+            return;
+        }
 
-        return $response->json();
+        $BackgroundTasks->put($backgroundtask_key, 1);
+    }
+
+    private function notifyUserSuccess(BackgroundTasks $BackgroundTasks, string $backgroundtask_key, array $woocommerce_states): void
+    {
+        if ($this->option('order')) {
+            return;
+        }
+
+        $BackgroundTasks->forget($backgroundtask_key);
+        $this->user->notify(FlashMessage::success('Die WooCommerce Bestellungen im Status <b>' . implode(', ', $woocommerce_states) . '</b> wurden importiert.', [
+            'background_tasks' => App::make(BackgroundTasks::class)->all(),
+        ]));
+    }
+
+    private function getOrderSourceIdsForStates(array $woocommerce_states): Collection
+    {
+        $states = collect($woocommerce_states)->map(function ($state) {
+            return Status::fromWooCommerceSlug($state)->value;
+        });
+
+        return Order::where('user_id', $this->user->id)
+            ->where('source_slug', ExternalType::WOOCOMMERCE->value)
+            ->whereIn('state', $states)
+            ->get()
+            ->pluck('source_id');
+    }
+
+    private function importNotImportedOrders(Collection $not_imported_orders)
+    {
+        if ($not_imported_orders->isEmpty()) {
+            return;
+        }
+
+        $WooCommerce = new \App\APIs\WooCommerce\WooCommerceOrder();
+
+        foreach ($not_imported_orders as $woocommerce_order_id) {
+            $response = $WooCommerce->order($woocommerce_order_id);
+
+            if (!$response->successful()) {
+                continue;
+            }
+
+            $order = WooCommerceOrderImporter::import($this->user->id, $response->json());
+        }
     }
 
     private function getWooCommerceOrders(): array
@@ -85,9 +116,12 @@ class ImportCommand extends Command
 
         if ($this->option('order')) {
             $response = $WooCommerce->order($this->option('order'));
-            return [
-                $response->json()
-            ];
+            if ($response->successful()) {
+                return [
+                    $response->json()
+                ];
+            }
+            return [];
         }
 
         if ($this->option('states')) {
